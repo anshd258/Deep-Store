9246aaa9a55123d8e488e6123933fe83f66b513b
Hemantthanna <Hemantthanna10@gmail.com>
2023-08-16 14:18:48 +0530

    common package ready to use

--- customer/lib/UI/screens/screen.setup.dart
+++ customer/lib/UI/screens/screen.setup.dart
@@ -1,11 +1,70 @@
 
+import 'package:common/common.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ScreenSetup extends StatelessWidget {
   const ScreenSetup({super.key, this.appBar});
   final PreferredSizeWidget? appBar;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomTabView(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: SvgPicture.asset('assets/icon.svg'),
+        title:  Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Welcome back!',
+                style: GoogleFonts.lato(
+                    color: const Color.fromRGBO(86, 86, 86, 1), fontSize: 16, fontWeight: FontWeight.w700 )),
+            Text('Rahul',
+                style: GoogleFonts.lato(
+                    color: const Color.fromRGBO(121, 121, 121, 1), fontSize: 19,fontWeight: FontWeight.w500)),
+          ],
+        ),
+      ),
+      screens: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            NavBarIcons.home,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            NavBarIcons.burger,
+          ),
+          label: 'Food',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            NavBarIcons.ride,
+          ),
+          label: 'Rides',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            NavBarIcons.account,
+          ),
+          label: 'Account',
+        ),
+      ],
+    );
   }
 }
--- customer/lib/main.dart
+++ customer/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'helpers/app.setup.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  
   runApp(const AppSetup());
 }
