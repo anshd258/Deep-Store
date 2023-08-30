9fe5206bcba249a02125822233529f283573516e
Hemantthanna <Hemantthanna10@gmail.com>
2023-08-31 01:22:09 +0530

    toggle button

--- customer/lib/presentation/screens/vehicle/rentalselect.dart
+++ customer/lib/presentation/screens/vehicle/rentalselect.dart
@@ -4,7 +4,6 @@ import 'package:customer/presentation/widgets/rentalitemcard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 class RentalSelect extends StatelessWidget {
   const RentalSelect({super.key});
   @override
@@ -13,10 +12,29 @@ class RentalSelect extends StatelessWidget {
 
     return BlocBuilder<RentalBloc, RentalState>(
       builder: (context, state) {
-                List<Rental>? rentalList =
-                    context.read<RentalBloc>().state.rentalList;
+        List<Rental>? rentalList = context.read<RentalBloc>().state.rentalList;
         return Column(
           children: [
+            Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.amber,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  LabeledToggleButton(
+                    label: 'Two wheelers only',
+                    initialValue: true,
+                    onChanged: (value) {},
+                  ),
+                  LabeledToggleButton(
+                    label: 'Four wheelers only',
+                    initialValue: true,
+                    onChanged: (value) {},
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               child: rentalList != null
                   ? ListView.builder(
@@ -27,8 +45,7 @@ class RentalSelect extends StatelessWidget {
                           name: rental.name,
                           price: rental.price,
                           images: rental.images,
-                          onClick: () {
-                          },
+                          onClick: () {},
                         );
                       })
                   : const Center(child: CircularProgressIndicator.adaptive()),
@@ -36,6 +53,50 @@ class RentalSelect extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class LabeledToggleButton extends StatefulWidget {
+  final String label;
+  final bool initialValue;
+  final ValueChanged<bool> onChanged;
+
+  LabeledToggleButton({
+    required this.label,
+    required this.initialValue,
+    required this.onChanged,
+  });
+
+  @override
+  _LabeledToggleButtonState createState() => _LabeledToggleButtonState();
+}
+
+class _LabeledToggleButtonState extends State<LabeledToggleButton> {
+  late bool _value;
+
+  @override
+  void initState() {
+    super.initState();
+    _value = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(widget.label),
+        SizedBox(width: 10),
+        Switch(
+          value: _value,
+          onChanged: (newValue) {
+            setState(() {
+              _value = newValue;
+            });
+            widget.onChanged(newValue);
+          },
+        ),
+      ],
     );
   }
 }
